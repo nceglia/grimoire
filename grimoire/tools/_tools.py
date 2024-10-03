47ec2cc626dda9122e803acbd48ecca9bf6bf92a
Nick Ceglia <nickceglia@gmail.com>
2024-10-02 22:20:45 -0400

    removed statsmodel

--- grimoire/tools/_tools.py
+++ grimoire/tools/_tools.py
@@ -1,6 +1,4 @@
 from scipy.stats import entropy
-import statsmodels.formula.api as smf
-import statsmodels.api as sm
 import numpy as np
 import pandas as pd
 import warnings
@@ -8,8 +6,6 @@ import gseapy as gp
 from scipy import sparse
 import scanpy as sc
 import collections
-import tqdm
-import scipy
 
 warnings.filterwarnings('ignore')
 
@@ -60,62 +56,21 @@ def deg_df(adata, category, adjp_thresh=0.001, lfc_thresh=0.25):
     df = df[df["logfoldchanges"].abs() > lfc_thresh]
     return df
 
-def gene_linear_regression(adata, key, coeff, significance=0.001):
-    cts = []
-    pvs = []
-    genes = []
-    coeffs = []
-    df = adata.obs.copy()
-    key_values = set(adata.ob[key].tolist())
-    key_coeff = key_values.index(coeff)
-    for gene in tqdm.tqdm(adata.var.index.tolist()):
-        df["Expression"] = adata.X[:,adata.var.index.tolist().index(gene)].T.todense().tolist()[0]
-        model = smf.ols('Expression ~ {}'.format(key), data=df).fit()
-        coef = model.params['Timepoint[T.{}]'.format(key_coeff)]  
-        p_value = model.pvalues['Timepoint[T.{}]'.format(key_coeff)]
-        coeffs.append(coef)
-        pvs.append(p_value)
-        genes.append(gene)
-    df = pd.DataFrame.from_dict({"Gene":genes,"Coefficient":coeffs, "P-value":pvs})
-    return df[df["P-value"] < significance]
-
-# def correlation(adata)
-#     mis = []
-#     pids  = []
-#     conds = []
-#     correlation = []
-#     gt = []
-#     for condition in set(adata.obs['subtype']):
-#         fdata = adata[adata.obs["subtype"]==condition]
-#         mat = []
-#         index = []
-#         for ct in tqdm.tqdm(set(fdata.obs['pid'])):
-#             xdata = fdata[fdata.obs["pid"]==ct]
-#             batf = xdata.X[:,xdata.var.index.tolist().index("KLF2")]
-#             ncells = len(xdata.obs.index)
-#             for gene in ["LEF1","SELL","CCR7","TCF7","IL7R","GZMK","LAG3","GZMA","GZMB"]:
-#                 target = xdata.X[:,xdata.var.index.tolist().index(gene.upper())]
-#                 if len(target) == 1: continue
-#                 x = []
-#                 y = []
-#                 for c,z in zip(batf, target):
-#                     if c > 0 or z > 0:
-#                         x.append(c)
-#                         y.append(z)
-#                 pxy, xedges, yedges = numpy.histogram2d(x, y, density=True)
-#                 pxy = pxy / pxy.sum()
-#                 px = np.sum(pxy, axis=1)
-#                 px = px / px.sum()
-#                 py = np.sum(pxy, axis=0)
-#                 py = py / py.sum()
-#                 px_py = px[:, None] * py[None, :]
-#                 nzs = pxy > 0
-#                 mi = np.sum(pxy[nzs] * numpy.log2((pxy[nzs] / px_py[nzs])))
-#                 c = scipy.stats.pearsonr(target,batf)
-#                 mis.append(mi)
-#                 correlation.append(c.statistic)
-#                 conds.append(condition)
-#                 pids.append(ct)
-#                 gt.append(gene)
-#     dfx = pd.DataFrame.from_dict({"Patient":pids,"Condition":conds,"r2":correlation,"MI":mis,"Target":gt})
-#     dfx
+# def gene_linear_regression(adata, key, coeff, significance=0.001):
+#     cts = []
+#     pvs = []
+#     genes = []
+#     coeffs = []
+#     df = adata.obs.copy()
+#     key_values = set(adata.ob[key].tolist())
+#     key_coeff = key_values.index(coeff)
+#     for gene in tqdm.tqdm(adata.var.index.tolist()):
+#         df["Expression"] = adata.X[:,adata.var.index.tolist().index(gene)].T.todense().tolist()[0]
+#         model = smf.ols('Expression ~ {}'.format(key), data=df).fit()
+#         coef = model.params['Timepoint[T.{}]'.format(key_coeff)]  
+#         p_value = model.pvalues['Timepoint[T.{}]'.format(key_coeff)]
+#         coeffs.append(coef)
+#         pvs.append(p_value)
+#         genes.append(gene)
+#     df = pd.DataFrame.from_dict({"Gene":genes,"Coefficient":coeffs, "P-value":pvs})
+#     return df[df["P-value"] < significance]
